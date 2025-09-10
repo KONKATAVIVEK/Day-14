9c96f2bcac8d498fe070855e72b7b652e761f457
KONKATAVIVEK <vivekkonkata@gmail.com>
2025-09-10 14:11:33 -0700

    arrays

---
+++ CommonStartOrEnd.java
@@ -0,0 +1,15 @@
+package com.codegnan.arrays;
+
+public class CommonStartOrEnd {
+	
+	    public static boolean commonEnd(int[] a, int[] b) {
+	        return a[0] == b[0] || a[a.length - 1] == b[b.length - 1];
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println(commonEnd(new int[]{1, 2, 3}, new int[]{7, 3}));       // true
+	        System.out.println(commonEnd(new int[]{1, 2, 3}, new int[]{7, 3, 2}));    // false
+	        System.out.println(commonEnd(new int[]{1, 2, 3}, new int[]{1, 3}));       // true
+	    }
+
+	}
